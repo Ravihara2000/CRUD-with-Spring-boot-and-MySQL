aa0297f28146f9446592eb62c91aa1c2cd5bafde
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-04-15 12:07:41 +0530

    dependency issue fix with v- 2.4.4

--- Backend/point-of-sale/src/main/java/com/springbootayacdemy/pointofsale/service/impl/ItemServiceImpl.java
+++ Backend/point-of-sale/src/main/java/com/springbootayacdemy/pointofsale/service/impl/ItemServiceImpl.java
@@ -1,31 +1,34 @@
 package com.springbootayacdemy.pointofsale.service.impl;
 
+import com.springbootayacdemy.pointofsale.config.ModelMapperConfig;
 import com.springbootayacdemy.pointofsale.dto.request.ItemSaveRequestDto;
-import com.springbootayacdemy.pointofsale.entity.Customer;
 import com.springbootayacdemy.pointofsale.entity.Item;
 import com.springbootayacdemy.pointofsale.repo.ItemRepo;
 import com.springbootayacdemy.pointofsale.service.ItemService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private ItemRepo itemRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
     @Override
     public String saveItem(ItemSaveRequestDto itemSaveRequestDto) {
-        Item item = new Item(
-                1,
-                itemSaveRequestDto.getItemName(),
-                itemSaveRequestDto.getMeasuringUnitType(),
-                itemSaveRequestDto.getBalanceQty(),
-                itemSaveRequestDto.getSupplierPrice(),
-                itemSaveRequestDto.getSellingPrice(),
-                true
-                );
 
-        itemRepo.save(item);
-
-        return itemSaveRequestDto.getItemName();
+        Item item = modelMapper.map(itemSaveRequestDto,Item.class);
+        if(itemRepo.existsById(item.getItemId())){
+            itemRepo.save(item);
+            return item.getItemName()+" saved successfully";
+        }else{
+            throw new DuplicateKeyException("Already added");
+        }
     }
 }
+// Item item = modelMapper.map(itemSaveRequestDto,Item.class);
